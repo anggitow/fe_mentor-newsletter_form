0e830a8e57933a3d2d72ebf33ba332ab675bdd53
Anggito Wicaksono <anggitov@gmail.com>
2023-07-27 00:28:44 +0700

    submit email

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        "tomato-ligt": "hsl(4, 100%, 95%)",
+        "dark-slate-grey": "hsl(234, 29%, 20%)",
+        "charcoal-grey": "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
